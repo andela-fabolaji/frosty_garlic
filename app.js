dae61a4b196dcbcf291fcddf7f6fd06b83175ae4
Femi Abolaji <femi.systems@gmail.com>
2019-03-16 00:48:05 +0100

    add input validation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 4000;
+const router = express.Router();
 
 app.set('view engine', 'ejs');
 
@@ -23,10 +24,24 @@ const calculatePrime = n => {
   return prime;
 };
 
-app.get('/', (req, res) => {
+const validate = function(req, res, next) {
+  if (req.query.n) {
+    const number = parseInt(req.query.n, 10);
+    if (isNaN(number) || number < 1 || number ? 10000) {
+      res.render('index', { error: 'Please submit a valid number between 1 and 10000' });
+      return;
+    }
+    req.query.n = number;
+  }
+  next();
+};
+
+router.get('/', validate, (req, res) => {
   const renderProps = req.query.n ? { n: req.query.n, prime: prime } : {};
   res.render('index', renderProps);
 });
+
+app.use('/', router);
 
 app.listen(port, err => {
   console.log('Listening on port' + port);
