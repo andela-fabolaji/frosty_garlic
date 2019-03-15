aba1f3a91ca97038e02b2a027ffd21e9524246ea
Femi Abolaji <femi.systems@gmail.com>
2019-03-16 00:53:16 +0100

    fix router

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const calculatePrime = n => {
 const validate = function(req, res, next) {
   if (req.query.n) {
     const number = parseInt(req.query.n, 10);
-    if (isNaN(number) || number < 1 || number ? 10000) {
+    if (isNaN(number) || number < 1 || number > 10000) {
       res.render('index', { error: 'Please submit a valid number between 1 and 10000' });
       return;
     }
