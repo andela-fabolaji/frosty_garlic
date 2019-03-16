016eb1fc5d0cbffe7eca6bee568a24b93b9f8062
Femi Abolaji <femi.systems@gmail.com>
2019-03-16 13:16:39 +0100

    implement memcache

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const memjs = require('memjs');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs');
-
+const mc = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
+  failover: true,
+  timeout: 1,
+  keepAlive: true
+});
 const calculatePrime = n => {
   let prime = 1;
   for (let i = n; i > 1; i--) {
@@ -35,8 +40,24 @@ const validate = function(req, res, next) {
   next();
 };
 
-app.get('/', validate, (req, res) => {
+app.get('/', validate, async (req, res) => {
   const n = req.query.n;
+  const primeKey = `prime.${n}`;
+  let prime;
+  let renderProps;
+  let useCached = true;
+  
+  if (n) {
+    prime = await mc.get(primeKey);
+    if (!prime) {
+      prime = await mc.set(primeKey, calculatePrime(n));
+      useCached = false;
+    }
+    renderProps = { n, prime: parseInt(prime), useCached };
+  } else {
+    renderProps = {};
+  }
+
   const renderProps = n ? { n, prime: calculatePrime(n) } : {};
   res.render('index', renderProps);
 });
